5b954dee1d2f51f8ef0b262772017d96f078537e
weixiumei <997817485@qq.com>
2016-09-11 21:11:24 +0800

    deleted

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 // let yeomanImage = require('../images/yeoman.png');
 //获取图片相关数据
-var imageDatas = require('../data/imageDatas.json');
+let imageDatas = require('../data/imageDatas.json');
 
 //利用自执行函数，将图片名信息转成图片URL路径信息。
 function genImageURL(imageDataArr){
@@ -16,16 +16,38 @@ function genImageURL(imageDataArr){
   }
   return imageDataArr;
 }
-
 imageDatas = genImageURL(imageDatas);
+
+var ImgFigure = React.createClass({
+  render: function(){
+    return (
+        <figure>
+          <img src={this.props.data.imageURL}
+            alt={this.props.data.title} />
+          <figcaption>
+            <h2>{this.props.data.title}</h2>
+          </figcaption>
+        </figure>
+      )
+  }
+});
 
 class AppComponent extends React.Component {
   render() {
+
+    var controllerUnits = [],
+        imgFigures = [];
+
+    // imageDatas.forEach(function(value){
+    //   imgFigures.push(<ImgFigure data={value}/>)
+    // });
     return (
         <section className="stage">
           <section className="img-sec">
+            {imgFigures}
           </section>
           <nav className="controller-nav">
+            {controllerUnits}
           </nav>
         </section>
     );
